3faacc71998e3fe1867b9a867a5fb0db65aae00b
iamaivilo <olivia.ma.acct@gmail.com>
2025-05-02 21:08:00 -0700

    Initial Commit

---
+++ Mathcat not Dad/Mathcat_not_DadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mathcat_not_DadApp.swift
+//  Mathcat not Dad
+//
+//  Created by Oh-Aivilo on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Mathcat_not_DadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mathcat not DadTests/Mathcat_not_DadTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mathcat_not_DadTests.swift
+//  Mathcat not DadTests
+//
+//  Created by Oh-Aivilo on 5/2/25.
+//
+
+import Testing
+@testable import Mathcat_not_Dad
+
+struct Mathcat_not_DadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
